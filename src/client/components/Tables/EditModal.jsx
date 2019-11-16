e3e6bcfec5d84e28abc42cf14d6e4ac618b2b9bb
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-11-16 18:43:42 +0300

    sacco edit UI

--- src/client/components/Tables/EditModal.jsx
+++ src/client/components/Tables/EditModal.jsx
@@ -13,28 +13,51 @@ const EditModal = props => {
       <Button outline color="warning" onClick={toggle}>
         <i className="fas fa-edit"></i>
       </Button>
-      <Modal isOpen={modal} toggle={toggle} className="modal-lg">
-        <ModalHeader toggle={toggle}>Edit {modalTitle}</ModalHeader>
-        <ModalBody>
+      <Modal isOpen={modal} toggle={toggle} className=" modal-lg">
+        {/* <ModalHeader toggle={toggle}>Edit {modalTitle}</ModalHeader> */}
+        <ModalBody className="modal-container">
           <div className="row">
             <div className="col-md-3 modal-left " align="center">
               <label className="">KISUMU SACCO</label>
             </div>
 
             <div className="col-md-9 modal-right">
+              <ModalHeader toggle={toggle} className="modal-btn"></ModalHeader>
               <label>Name</label>
-              <input type="text" placeholder="sacco name" />
+              <input type="text" />
+              <label>Phone:</label>
+              <input type="text" />
+              <label>Email:</label>
+              <input type="text" />
+              <label>Address:</label>
+              <input type="text" />
+              <label>Manager:</label>
+              <input type="text" />
+              <label>Code:</label>
+              <input type="text" />
+              <label>Registration Number:</label>
+              <input type="text" />
+              <label>Date Founded:</label>
+              <input type="text" />
+              <label>Status:</label>
+              <input type="text" />
+
+              <ModalFooter className="modal-btn">
+                <Button color="primary" onClick={toggle}>
+                  Save
+                </Button>
+              </ModalFooter>
             </div>
           </div>
         </ModalBody>
-        <ModalFooter>
+        {/* <ModalFooter>
           <Button color="primary" onClick={toggle}>
             Submit
           </Button>{" "}
           <Button color="secondary" onClick={toggle}>
             Cancel
           </Button>
-        </ModalFooter>
+        </ModalFooter> */}
       </Modal>
     </>
   );
